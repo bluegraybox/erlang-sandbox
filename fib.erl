62f665b2fc62f622cf02183bf51898ad85ce4f49
Colin MacDonald <colin.r.macdonald@gmail.com>
2008-08-05 11:23:05 -0400

    Tighter implementation; fixed sequence.

--- fib.erl
+++ fib.erl
@@ -2,11 +2,8 @@
 -export([calc/1]).
 
 calc( 0 ) -> [ 0 ];
-calc( 1 ) -> [ 1 ];
+calc( 1 ) -> [ 1, 0 ];
 calc( X ) ->
-    L = calc( X - 1 ),
-    [ Pre1 | T ] = L,
-    [ Pre2 | T2 ] = calc( X - 2 ),
-    Val = Pre1 + Pre2,
-    [ Val | L ].
+    [ Pre1, Pre2 | T1 ] = calc( X - 1 ),
+    [ Pre1 + Pre2 | [ Pre1 | [ Pre2 | T1 ] ] ].
 
